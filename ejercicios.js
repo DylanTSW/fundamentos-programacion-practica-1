5dc70dd263fd2a785c2f037ced58d285d01af556
DylanTSW <dylan.vazquez@tecdesoftware.edu.mx>
2025-11-11 15:43:52 -0600

    Ejercicio 5.2 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -316,13 +316,13 @@ function crearMatriz(filas, columnas) {
 function sumaMatriz(matriz) {
   // TODO: Suma todos los elementos de la matriz
   // NecesitarÃ¡s dos bucles: uno para las filas y otro para las columnas
-  let total = 0;
-  for (let i = 0; i < matriz.length; i++) {
-    for (let j = 0; j < matriz[i].length; j++) {
-      total += matriz[i][j];
-    }
-  }
-  return total;
+  let total = 0; 
+  for (let i = 0; i < matriz.length; i++) { 
+    for (let j = 0; j < matriz[i].length; j++) { 
+      total += matriz[i][j]; 
+    } 
+  } 
+  return total; 
 }
 
 /**
