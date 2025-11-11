1cc2302cda2d714408bb1da4d102ea59ecf4c5a5
DylanTSW <dylan.vazquez@tecdesoftware.edu.mx>
2025-11-11 15:41:57 -0600

    Ejercicio 4.3 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -238,13 +238,13 @@ function promedioArray(numeros) {
 function encontrarMaximo(numeros) {
   // TODO: Encuentra y retorna el nÃºmero mÃ¡s grande
   // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle
-  let max = numeros[0];
-  for (let i = 1; i < numeros.length; i++) {
-    if (numeros[i] > max) {
-      max = numeros[i];
-    }
-  }
-  return max;
+  let max = numeros[0]; 
+  for (let i = 1; i < numeros.length; i++) { 
+    if (numeros[i] > max) { 
+      max = numeros[i]; 
+    } 
+  } 
+  return max; 
 }
 
 /**
